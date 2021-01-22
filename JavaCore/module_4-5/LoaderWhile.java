4abf206c0fcfe04fb06eaef515cc44fac8bd60c3
arpharutyunyan <arpine.harutyunyana@gmail.com>
2021-01-22 20:02:50 +0400

    Java Core

---
+++ JavaCore/module_4-5/LoaderWhile.java
@@ -0,0 +1,11 @@
+public class LoaderWhile {
+    public static void main(String[] args) {
+        int i = 45;
+        while (i != 350) {
+            i++;
+        }
+
+        System.out.println("Value i = " + i);
+    }
+}
+
---
+++ JavaCore/module_4-5/Regexp.java
@@ -0,0 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regexp {
+
+    public static void main(String[] args) {
+
+        String text = "2000 2001 1885 2020";
+        Pattern pattern = Pattern.compile("20\\d{2}");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            System.out.println(text.substring(start, end));
+        }
+
+    }
+
+}
