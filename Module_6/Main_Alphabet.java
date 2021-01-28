ea2f1bde98078751b520fc992adac752beb18656
arpharutyunyan <arpine.harutyunyana@gmail.com>
2021-01-28 18:13:54 +0400

    Java Core Module 6(Unit 3)

---
+++ Module_6/Main_Alphabet.java
@@ -0,0 +1,25 @@
+package Module_6;
+
+public class Main_Alphabet {
+    public static void main(String[] args) {
+    /*
+    Make Alphabet class,
+    so that this code and *similar ones* work correctly
+    */
+        Alphabet alphabet = new Alphabet();
+        alphabet.setW(5);
+        System.out.println(alphabet.getSum());
+        alphabet.setA(5);
+        alphabet.setS(7);
+        alphabet.setD(7);
+        System.out.println(alphabet.getSum());
+
+    /*
+    This fragment of code has to output
+
+    5
+    24
+
+    */
+    }
+}
